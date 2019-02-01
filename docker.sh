6da2c00ab4ee5d991a609bb592101dbaefad233b
Kamil Wronka <Kamil.Wronka@rossmann.com.pl>
2019-02-01 12:05:42 +0100

    changed sh script

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
 sudo docker pull kamilwronka7/messenger-backend:latest
 sudo docker stop messenger-backend
 sudo docker rm messenger-backend
-sudo docker run --name messenger-backend -d kamilwronka7/messenger-backend
+sudo docker run --name messenger-backend -d -p 80:4000 kamilwronka7/messenger-backend

26822db52b8ba4a248fc363b8db04a6ff465bfd8
Ubuntu <ubuntu@ip-172-31-36-153.eu-central-1.compute.internal>
2019-11-05 14:39:21 +0000

    updated script

--- docker-startup-script.sh
+++ docker-startup-script.sh
@@ -3,6 +3,6 @@ IMAGE_TAG=$2;
 IMAGE_NAME=$3;
 
 sudo docker pull $REPO_NAME:$IMAGE_TAG
-sudo docker stop $REPO_NAME
-sudo docker rm $REPO_NAME
+sudo docker stop $IMAGE_NAME
+sudo docker rm $IMAGE_NAME
 sudo docker run --network host --name $IMAGE_NAME -d -p 80:80 $REPO_NAME
